950eace5204570e31e74c79d7930548ec261aaa2
Niansheng <victor.ns.liu@outlook.com>
2014-04-11 10:25:16 +1200

    setup test environment.

--- project/apps/archive/tests.py
+++ project/apps/archive/tests.py
@@ -2,7 +2,29 @@
 Archive App: tests
 """
 from django.test import TestCase
+from django.contrib.auth.models import User
+from mock import Mock
 
+
+from .models import *
 # Create your tests here.
 
-class 
+class CompanyDetailTest(TestCase):
+    """
+    Test every behaviour CompanyDetail has.
+    """
+    @classmethod
+    def setUp(self):
+        self.mocked_com = Mock(spec=CompanyDetail)
+        self.mocked_com.pk = 1
+        self.mocked_com.company_name = "Newsletter"
+        self.mocked_com.company_country = "India"
+        self.mocked_com.domain_names = "newsletterarchive.com"
+        self.mocked_com.subdomain_names = "sub.newsletterarchive.com"
+        self.mocked_com.company_tags = "Information"
+        self.mocked_com.added_by = Mock(spec=User)
+
+    def test_companydetail_unicode(self):
+        self.assertEqual(1,1)
+        #self.assertEqual(self.__unicode__(), "Company:%d, %s - %s" %(self.mocked_com.pk, 
+        #    self.mocked_com.company_name, self.mocked_com.domain_names))
--- project/project/settings.py
+++ project/project/settings.py
@@ -4,3 +4,17 @@ except ImportError:
     pass
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '9!7nw)$=9qv*qyrtesy#&_2+$*m0tv6+2cvdz+rzigwz3%z9f&'
+
+import sys
+
+if 'test' in sys.argv:
+    # Use nose to run all tests
+    TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+    INSTALLED_APPS += ('django_nose',)
+    # Tell nose to measure coverage on these apps
+    NOSE_ARGS = [
+        '--with-coverage',
+        '--cover-package=archive,',
+        '--verbosity=2',
+    ]
+
